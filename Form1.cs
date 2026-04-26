4319f9ed28587772e2dc2356a5427b9954b546e4
CristianSantos <CristianSantos@PSY-E113>
2026-04-26 12:34:19 -0500

    Cracion del login

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace wSistemaMantenimientoPreventivoCorrectivo
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
